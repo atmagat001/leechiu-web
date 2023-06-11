fd3e0d71b62412dee3daed5901ef19303c8035f9
Alvin Magat <atmagat@gmail.com>
2023-06-11 12:41:15 +0800

    atm

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,65 @@ import React from 'react'
 
 function Services() {
   return (
-    <div className='services'>
+    <div className='services' id='services'>
         <div className="services-container">
-            <h1>Our Services</h1>
+            <h1>We help you make the most out of your Real Estate Asset</h1>
+            <div className='services-underline'></div>
+            <p>OUR SERVICES</p>
+            <div className="card-container cont-row">
+            
+              <div className="card leasing">
+                <div className="card-background">
+                  <h2>Office Leasing</h2>
+                  <p>Our <strong>Tenant Representation</strong> specialists facilitate lease acquisitions, renewals and restructuring for their clients. These services are provided together with various tools that help our clients determine the best real estate strategy.</p>
+                  <p>With <strong>Landlord Representation</strong> We help landlords optimize their properties’ potential and reach their objectives by achieving stable occupancy and quality tenant mix.</p>
+                  <a href="">Learn More</a>
+                </div>
+              
+              </div>
+
+              <div className="card investment">
+                <div className="card-background">
+                  <h2>Investment Sales</h2>
+                  <p>We possess the deepest bench of consultants in terms of market tenure and experience, deal volume and total peso value transacted.</p>
+                  <a href="">Learn More</a>
+                </div>
+              
+              </div>
+
+              <div className="card research">
+                <div className="card-background">
+                  <h2>Research and Consultancy</h2>
+                  <p>Our Research Team offers a full suite of consulting services that cover market research and data analytics, project conceptualization and feasibility, and financial modelling.</p>
+                  <a href="">Learn More</a>
+                </div>
+               
+              </div>
+              <div className="card hotels">
+                <div className="card-background">
+                  <h2>Hospitality, Tourism and Leisure</h2>
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae eaque illo illum ipsum deleniti magnam molestias minima totam, perspiciatis error, sapiente voluptatum corrupti sed accusamus sequi rerum officiis nemo natus.</p>
+                  <a href="">Learn More</a>
+                </div>
+                
+              </div>
+              <div className="card residential">
+                <div className="card-background">
+                  <h2>Residential</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae distinctio dolore tenetur hic illum asperiores maiores sed, sequi modi debitis provident dolorem eligendi repudiandae enim ratione nulla labore id facilis!</p>
+                  <a href="">Learn More</a>
+                </div>
+                
+              </div>
+              <div className="card condo">
+                <div className="card-background">
+                  <h2>Project Selling</h2>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus aliquid expedita sapiente quaerat, unde accusamus maiores consequuntur cumque nihil porro? Numquam fugiat odio dolorem doloribus sapiente unde fugit aspernatur dolorum.</p>
+                  <a href="">Learn More</a>
+                </div>
+               
+              </div>
+            </div>
         </div>
 
 
